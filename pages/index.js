a297e95226c8b94e84752246bca6e56d8e683fe3
Felix Koch <mail@felixkoch.de>
2019-02-07 20:22:22 +0100

    index steht

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import "../scss/styles.scss";
 
 import Head from "next/head";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default () => (
   <div>
@@ -10,22 +10,56 @@ export default () => (
         href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet"
       />
+      <script
+        defer
+        src="https://use.fontawesome.com/releases/v5.7.1/js/all.js"
+        integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7"
+        crossorigin="anonymous"
+      />
+
       <title>Felix Koch</title>
     </Head>
-    <div class="container">
-      <h1>
-        Felix Koch
-        <br />
-        <small class="text-muted">Vater, Entwickler, Imker</small>
-      </h1>
-      <p>
-        <Link ><a href="">GutHub</a></Link>
-        <br />
-        Xing
-      </p>
-      
+    <div className="container">
+      <div className="d-md-flex justify-content-center">
+        <div className="p-3">
+          <img
+            src="/static/web.jpg"
+            alt="Felix Koch"
+            className="rounded-circle profilbild"
+          />
+        </div>
+        <div className="p-3">
+          <h1>
+            Felix Koch
+            <br />
+            <small className="text-muted">Vater, Entwickler, Imker</small>
+          </h1>
+          <div className="d-flex">
+            <a href="https://github.com/felixkoch" className="mr-4">
+              <i class="fab fa-github mr-2" />
+              GitHub
+            </a>
+
+            <a href="https://www.xing.com/profile/Felix_Koch80/">
+              <i class="fab fa-xing mr-2" />
+              XING
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
 
-    <footer class="fixed-bottom"><div className="container">Impressum Datenschutz</div></footer>
+    <footer className="fixed-bottom">
+      <div className="container">
+        <div className="d-flex p-2 justify-content-end small">
+          <Link href="/impressum">
+            <a className="mr-4 text-muted">Impressum</a>
+          </Link>
+          <Link href="/datenschutz">
+            <a className="text-muted">Datenschutz</a>
+          </Link>
+        </div>
+      </div>
+    </footer>
   </div>
 );
