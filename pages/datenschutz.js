144c4d86aa37adff445808764227916f87cdb519
Felix Koch <mail@felixkoch.de>
2019-02-08 20:04:55 +0100

    Seiten für Impressum und Datenschutz

---
+++ pages/datenschutz.js
@@ -0,0 +1,9 @@
+import Page from "../components/Page"
+
+export default () => (
+  <Page>
+    <div className="container">
+        <h1>Datenschutz</h1>
+    </div>
+  </Page>
+)
---
+++ pages/impressum.js
@@ -0,0 +1,9 @@
+import Page from "../components/Page"
+
+export default () => (
+  <Page>
+    <div className="container">
+        <h1>Impressum</h1>
+    </div>
+  </Page>
+)
