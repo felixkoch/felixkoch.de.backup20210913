5ee0432c7bdf5a9fa7eca0c5201234527d62d028
Felix Koch <mail@felixkoch.de>
2019-02-16 19:23:37 +0100

    google analytics

--- components/Page.js
+++ components/Page.js
@@ -3,6 +3,10 @@ import "../scss/styles.scss";
 import Header from "./Header";
 import Footer from "./Footer";
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-134614561-1');
+ReactGA.pageview("/");
+
 export default props => (
   <div>
     <Header title={props.title} noHeader={props.noHeader} />
